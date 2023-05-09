fd0407a1d6b2e6a103beed1dcb6eff9bedbc2d3c
FacundoPougy <facu.pougy@gmail.com>
2023-05-08 23:03:36 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = procces.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Servidor en el puerto " + port);
+});
