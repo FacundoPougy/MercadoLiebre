1af0b9619ee1516c9b08ca1ce813fcafd6be60ae
FacundoPougy <129338219+FacundoPougy@users.noreply.github.com>
2023-05-08 23:41:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = procces.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -17,6 +17,6 @@ app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/login.html"));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Servidor en el puerto " + port);
 });
